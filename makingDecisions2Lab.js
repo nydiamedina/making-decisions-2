0cccd02bf4aeeeb4a0a23462c046aff9a2a86b6e
Nydia Medina <nvmedina29@gmail.com>
2021-11-18 06:44:32 -0600

    Submitting problems 8 and 9

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -119,6 +119,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
+for (let i = 0; i < listOfNumbers.length; i++) {
+  if (listOfNumbers[i] % 3 === 0) {
+    console.log(`${listOfNumbers[i]} is divisible by 3.`);
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -131,6 +136,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+for (i = letters.length - 1; i >= 0; i--) {
+  console.log(letters[i]);
+}
 
 ////////// Advanced Problems //////////
 
