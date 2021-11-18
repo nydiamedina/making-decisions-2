185f5cbb8b1b2f450fb1aff67f7dc9763497cf02
Nydia Medina <nvmedina29@gmail.com>
2021-11-18 06:55:26 -0600

    Submitting problem 11

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -206,3 +206,14 @@ switch (letterGrade) {
 */
 
 //Code Here
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('Devmountain');
+  } else if (i % 3 === 0) {
+    console.log('Dev');
+  } else if (i % 5 === 0) {
+    console.log('Mountain');
+  } else{
+    console.log(i);
+  }
+}
